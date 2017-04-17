d987a4ba6dbcb0fdbd60b3d3ee72c61d724f5434
Welliton Souza <well309@gmail.com>
2017-04-17 17:14:15 +0000

    Deleted old files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GA4GHclient@128770 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/as.VCFHeader.R
+++
@@ -1,7 +0,0 @@
-#' @export as.VCFHeader
-as.VCFHeader <- function(variantSet)
-{
-    info <- with(variantSet$metadata, DataFrame(Number = number, Type = type,
-        Description = description, row.names = sub("^INFO\\.", "", key)))
-    VCFHeader(header = DataFrameList(INFO = info))
-}
